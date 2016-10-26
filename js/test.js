54e9142e9cbebaca5fbbe9753f4f9be0ec68193d
Carlos Alberto Mata Gil <CeuxDruman@gmail.com>
2016-10-26 16:01:21 +0200

    Soporte para ceros

--- js/test.js
+++ js/test.js
@@ -4,8 +4,18 @@ function testSum2Num() {
 	}
 }
 
+function testSumWithZero() {
+	if (sum(3, 0) != 3) {
+		alert("Error")
+	}
+	if (sum(0, 3) != 3) {
+		alert("Error")
+	}
+}
+
 function sum(a, b) {
-	return 6;
+	return a+b;
 }
 
 testSum2Num();
+testSumWithZero();
